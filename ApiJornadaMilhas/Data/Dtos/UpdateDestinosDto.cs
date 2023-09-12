cbc1ab45c6ef578622d593be405f4b4797b22e4e
afranio-viana <afranio.esquerdo.viana@gmail.com>
2023-09-12 15:39:28 -0400

    Adicionando novos campos nos models de Destino

--- ApiJornadaMilhas/Data/Dtos/UpdateDestinosDto.cs
+++ ApiJornadaMilhas/Data/Dtos/UpdateDestinosDto.cs
@@ -1,9 +1,15 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiJornadaMilhas.Data.Dto;
 
 public class UpdateDestinosDto
 {
-    public string Foto {get; set;}
+    public string Foto1 {get; set;}
+    public string Foto2 {get; set;}
     public string Nome {get; set;}
     public float Preco {get; set;}
+    [MaxLength(160, ErrorMessage = "O Campo Meta deve possuir no máximo 160 caracters")]
+    public string Meta {get; set;}
+    public string? TextoDescritivo {get; set;}
 }
--- ApiJornadaMilhas/Models/Destinos.cs
+++ ApiJornadaMilhas/Models/Destinos.cs
@@ -1,4 +1,5 @@
 
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -9,7 +10,11 @@ public class Destinos
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id {get; set;}
-    public string Foto {get; set;}
+    public string Foto1 {get; set;}
+    public string Foto2 {get; set;}
     public string Nome {get; set;}
     public float Preco {get; set;}
+    [MaxLength(160, ErrorMessage = "O Campo Meta deve possuir no máximo 160 caracters")]
+    public string Meta {get; set;}
+    public string? TextoDescritivo {get; set;}
 }
